b16183fd8ac1edd7c23316aa4c80ebf22695d2b5
jkeiffer1 <saros82@hotmail.com>
2017-07-29 18:12:18 -0400

    login page done. success message carried into name page successfully

--- calcapp.rb
+++ calcapp.rb
@@ -1,20 +1,21 @@
 require "sinatra"
-require_relative "calfunctions.rb"
+
 
 get '/' do
-	erb :login
+	message = params[:message]
+	erb :login, locals:{message: message}
 end
 
 post '/login' do
-	correct_login:{jkeiffer: 1234}
+	correct_login = {jkeiffer: "1234", scstew: "4321", ldeems: "1233"}
 	username = params[:username]
 	password = params[:password]
 	message = "Incorrect Username or Password, please try again."
 	success = "Login Successful"
-	correct_login.each do
-		if username == key && password == value
+	correct_login.each_pair do |key, value|
+		if username == key.to_s && password == value
 			redirect '/name?success=' + success
-		elsif username == key
+		elsif username == key.to_s
 			message = "Incorrect password!"
 			redirect '/?message=' + message
 		elsif password == value
@@ -22,6 +23,19 @@ post '/login' do
 			redirect '/?message=' + message
 		else
 			message = "Incorrect username and password!"
-
-	 	redirect '/?message=' + message
+			redirect '/?message=' + message
+		end
+	
 	end
+end
+
+get '/name' do
+	success = params[:success]
+	erb :names, locals:{success: success}
+end
+
+post '/names' do
+	firstname = params[:firstname]
+	lastname = params[:lastname]
+	success = params[:success]
+end
